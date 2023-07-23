ab673e5984cbf858d6add40a2a727b136c274ad3
Chandrakant Bhirud <chandubhirud@yahoo.com>
2023-07-23 21:20:03 +0100

    Test

---
+++ cypress/integration/feature/personalDetails.feature
@@ -0,0 +1,14 @@
+Feature: Login to BBC website and update the postcode
+
+    Scenario:Login to BBC Website
+        Given User launch the BBC website
+        When User enter "chandubhirud@yahoo.com", "login2bbc" and click on submit button
+        Then User validate the home page
+
+
+    Scenario:Login to BBC Website and update the postcode
+        Given User launch the BBC website
+        When User enter "chandubhirud@yahoo.com", "login2bbc" and click on submit button
+        Then user clicks on Account and settings
+        And user click on Continue to Settings button
+        Then user update the postcode
--- cypress/support/step_definitions/login.js
+++ cypress/support/step_definitions/login.js
@@ -1,11 +1,21 @@
-import { Given, When } from "@badeball/cypress-cucumber-preprocessor"
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
 
 Given('User launch the BBC website', () => {
     cy.visit('https://account.bbc.com/signin')
 })
 
-When('User login to the Website', () => {
-    cy.get('input[id=user-identifier-input]').type('chandubhirud@yahoo.com')
-    cy.get('input[id=password-input]').type('login2bbc')
+When('User enter {string}, {string} and click on submit button', (username,password) => {
+    cy.get('input[id=user-identifier-input]').type(username)
+    cy.get('input[id=password-input]').type(password)
     cy.get('button[type=submit]').click()
+   
 })
+
+Then('User validate the home page', () => {
+        cy.origin('https://www.bbc.co.uk', () =>{
+        cy.get('.ssrcss-13ox91y-NavigationLink-AccountLink').should('be.visible')
+     
+    })
+})
+
+
